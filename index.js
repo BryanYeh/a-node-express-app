7438d4040fb74b4c11c6e1d933235bf76950bf81
Bryan Yeh <bbryanyeh@gmail.com>
2023-08-27 10:08:56 -0700

    add: homepage route

--- index.js
+++ index.js
@@ -2,24 +2,41 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const { ServerError } = require('./exceptions/ServerError')
+const handlebars = require('express-handlebars')
+const routes = require('./routes')
 
 const errorHandler = require('./middlewares/errorHandler.middleware.js')
+
+// use handlebars templating engine
+// use .hbs extension instead of handlebars, can change ".hbs" to ".handlebars"
+app.engine(
+    '.hbs',
+    handlebars.engine({
+        extname: '.hbs',
+        defaultLayout: 'app',
+        helpers: {
+            route(path) {
+                return process.env.APP_URL + ':' + process.env.APP_PORT + path
+            },
+            config(name) {
+                return process.env[name]
+            },
+            old(obj, key) {
+                return obj ? obj[key] : ''
+            },
+        },
+    })
+)
+app.set('view engine', '.hbs')
+app.set('views', __dirname + '/views')
+app.use(express.static('public'))
 
 // Constants
 const PORT = process.env.DOCKER_PORT || 3000
 const HOST = '0.0.0.0'
 
 // App
-app.get('/', async (req, res, next) => {
-    // res.send('Hello World!')
-    try {
-        // res.send('hello')
-        throw new ServerError()
-    } catch (error) {
-        return next(error)
-    }
-})
+app.use('/', routes)
 
 app.use(errorHandler.logger)
 app.use(errorHandler.responder)
